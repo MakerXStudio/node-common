197b6d2cf85bf34e600038ce23f805c29e35d2fa
Sam Curry <sam.curry@makerx.com.au>
2022-09-28 16:22:43 +0800

    don't worry about coverage for now

--- jest.config.ts
+++ jest.config.ts
@@ -4,8 +4,6 @@ const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/**/*.spec.ts'],
-  coverageProvider: 'v8',
-  coverageDirectory: './coverage',
   transform: {
     '<regex_match_files>': [
       'ts-jest',
