e9e8a308fa49a72d28b7d3b28d72c59fbaa8e5a9
Sam Curry <sam.curry@makerx.com.au>
2022-09-28 16:14:58 +0800

    lets go

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,8 @@ import type { Config } from '@jest/types'
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  coverageProvider: 'v8',
+  coverageDirectory: './coverage',
   transform: {
     '<regex_match_files>': [
       'ts-jest',
