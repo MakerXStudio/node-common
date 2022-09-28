1021819034ff61853164d7acffa362c491d91efe
Sam Curry <sam.curry@makerx.com.au>
2022-09-28 16:21:45 +0800

    restore deleted line

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from '@jest/types'
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ['**/**/*.spec.ts'],
   coverageProvider: 'v8',
   coverageDirectory: './coverage',
   transform: {
