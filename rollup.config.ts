40769596a6ae606d64664d15aa013b522ecdc1a5
Tristan Menzel <tristanmenzel@gmail.com>
2024-02-14 10:36:48 -0800

    build: Run ts-toolkit init to standardise build process

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,33 +1,42 @@
 import nodeResolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
-import { RollupOptions } from 'rollup'
+import json from '@rollup/plugin-json'
+import type { RollupOptions } from 'rollup'
 
 const config: RollupOptions = {
-  input: 'src/index.ts',
+  input: ['src/index.ts'],
   output: [
     {
       dir: 'dist',
       format: 'cjs',
-      entryFileNames: '[name].cjs',
+      entryFileNames: '[name].js',
+      exports: 'named',
       preserveModules: true,
+      sourcemap: true,
     },
     {
       dir: 'dist',
       format: 'es',
+      exports: 'named',
       entryFileNames: '[name].mjs',
       preserveModules: true,
+      sourcemap: true,
     },
   ],
   treeshake: {
     moduleSideEffects: false,
     propertyReadSideEffects: false,
   },
-  // no deps, normally external might be all your deps: [...Object.keys(pkg.dependencies ?? {}), ...Object.keys(pkg.peerDependencies ?? {})], import pkg from './package.json'
+  // List modules that should not be included in the output bundle (ie. they should remain external dependencies)
+  external: [/node_modules/],
   plugins: [
     typescript({
       tsconfig: 'tsconfig.build.json',
     }),
+    commonjs(),
     nodeResolve(),
+    json(),
   ],
 }
 
